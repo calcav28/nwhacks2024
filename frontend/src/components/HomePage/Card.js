07184243d951224cd3ebc3a2b0766be0816590e3
Andrew Fenton <andrewfenton898@gmail.com>
2024-01-21 00:41:47 -0800

    updated card

--- frontend/src/components/HomePage/Card.js
+++ frontend/src/components/HomePage/Card.js
@@ -1,10 +1,27 @@
-import React from "react";
+import {React , useState} from "react";
 import '../../styles/CardStyles.css';
+import logo from '../../logo.svg';
 
 export default function Card(props) {
+    let hasCounted;    
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        if (~hasCounted) {
+            hasCounted = true;
+            setCount(count + 1);
+        } else {
+            hasCounted = false;
+            setCount(count - 1);
+        }
+    }
+    
     return (
-        <button className="styled" size="lg">
-            <img src={props.imgUrl} alt="Event Selection" className="selectionPhoto" />
-        </button>
+        <div>
+            <button className="styled" size="lg">
+                <img src={props.imgUrl} alt="Event Selection" className="selectionPhoto" />
+            </button>
+        </div>
+
     );
 }
